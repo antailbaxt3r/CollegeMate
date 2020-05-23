5f46d51a5ff695f2ed6654d1bf7dfcc5a79fea6a
Arjun Bajpai <arjunbajpaiwork@gmail.com>
2020-05-23 06:20:28 +0530

    Removed backend files from this repo

--- backend/routes/signin.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var googleSignin = require("../controllers/googleSigninCtrl");
-
-router.post('/google/signin', googleSignin.checkUserGoogle)
-
-module.exports = router
