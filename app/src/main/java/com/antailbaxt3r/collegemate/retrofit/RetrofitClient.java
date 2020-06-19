5163648c86e37dc802e8e5cdbb67e930ebab664a
OmSingh <omsingh6903@gmail.com>
2020-06-19 15:05:11 +0530

    Changed Web api base url

--- app/src/main/java/com/antailbaxt3r/collegemate/retrofit/RetrofitClient.java
+++ app/src/main/java/com/antailbaxt3r/collegemate/retrofit/RetrofitClient.java
@@ -12,7 +12,7 @@ public class RetrofitClient {
 
     public static APIInterface getClient() {
         if (apiInterface == null) {
-            String BASE_URL = "http://192.168.0.104:4193/api/";
+            String BASE_URL = "https://collegemate-api.herokuapp.com/api/";
             Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .client(getHttpClient())
